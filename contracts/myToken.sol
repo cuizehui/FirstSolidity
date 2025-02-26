8d72d8087a544343a05a5a6bfe023a6464da2160
zehui.cui <zehui.cui5@transsion.com>
2025-02-26 11:49:35 +0800

    feature: 增加ntf合约

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -1,10 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        _mint(msg.sender, initialSupply * (10 ** decimals()));
+contract NELANFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIds;  
+
+    // 向 ERC721 构造函数传递代币名称和符号
+    /**
+     * @dev 合约构造函数，初始化 ERC721 代币的名称和符号，同时初始化 ERC721URIStorage 和 Ownable 合约
+     */
+    constructor() 
+        ERC721("NELANFT", "NELA") 
+        ERC721URIStorage() 
+        Ownable() 
+    {
+        _tokenIds = 0;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        require(recipient != address(0), "Invalid recipient address");  // 检查接收者地址是否有效
+        require(bytes(tokenURI).length > 0, "Invalid token URI");  // 检查元数据 URI 是否为空
+
+        _tokenIds++;
+        uint256 newItemId = _tokenIds;
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
     }
 }
