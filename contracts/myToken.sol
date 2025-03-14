23c9a4fb89622b8059286694768cb0188a0a14d5
nela.cui <454776824@qq.com>
2025-03-14 16:15:03 +0800

    feature;增加最大供应量

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NELANFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIds;  
+    uint256 public constant MAX_SUPPLY = 1000; // 设置最大供应量
 
     // 向 ERC721 构造函数传递代币名称和符号
     /**
@@ -13,6 +14,7 @@ contract NELANFT is ERC721URIStorage, Ownable {
      */
     constructor() 
         ERC721("NELANFT", "NELA") 
+        
         ERC721URIStorage() 
         Ownable(msg.sender) 
     {
@@ -25,11 +27,12 @@ contract NELANFT is ERC721URIStorage, Ownable {
     {
         require(recipient != address(0), "Invalid recipient address");  // 检查接收者地址是否有效
         require(bytes(tokenURI).length > 0, "Invalid token URI");  // 检查元数据 URI 是否为空
-
+        require(_tokenIds < MAX_SUPPLY ,  "Max supply reached");  // 检查是否达到最大供应量     
         _tokenIds++;
         uint256 newItemId = _tokenIds;
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
     }
+
 }
